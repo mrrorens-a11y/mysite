e3d833fa2a312b2525eb77970eb0b9728baf3c9b
mrrorens-a11y <mrrorens@yahoo.co.jp>
2026-03-02 22:36:51 +0900

    switch to browser rakuten api

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 app = Flask(__name__)
 
 RAKUTEN_APP_ID = "99c6687c-934b-461a-91d2-cccac125bba8"
+RAKUTEN_ACCESS_KEY = "pk_d9C17gWx15sJvd3zfV6c1vXvKjrsSbd8rFSOOf1K0jY"  # ← これが accessKey
+RAKUTEN_AFFILIATE_ID = "516c5cc9.1bfc6c7d.516c5cca.f7d5de3e"
 
 @app.route("/")
 def index():
@@ -15,20 +17,19 @@ def search():
 
     url = "https://openapi.rakuten.co.jp/engine/api/Travel/KeywordHotelSearch/20170426"
 
+    headers = {
+        "X-Rakuten-Application-Id": RAKUTEN_APP_ID,
+        "X-Rakuten-Access-Key": RAKUTEN_ACCESS_KEY
+    }
+
     params = {
         "format": "json",
         "keyword": keyword,
-        "applicationId": RAKUTEN_APP_ID,
+        "affiliateId": RAKUTEN_AFFILIATE_ID,
         "hits": 20
     }
 
-    res = requests.get(url, params=params)
-
-    print("========== 楽天からのレスポンス ==========")
-    print(res.status_code)
-    print(res.json())
-    print("===========================================")
-
+    res = requests.get(url, headers=headers, params=params)
     return jsonify(res.json())
 
 if __name__ == "__main__":
